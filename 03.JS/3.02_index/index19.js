562962a9a0f15b2e47097134f137cd6e35c282b7
ningdol0711 <wnsrb2697@gmail.com>
2024-03-02 11:22:35 +0900

    auto function, object

---
+++ 03.JS/3.02_index/index19.js
@@ -0,0 +1,31 @@
+// 객체 선언 방법 3가지
+
+// 1.
+let person1 = new Object();
+person1.name = "mike";
+person1.age = 36;
+person1.sayhi = function() {
+  console.log(`Hello ${this.name}`);
+}
+console.log(person1);
+
+// 2. 리터럴
+let person2 = {
+  name : "chris",
+  age : 28,
+  sayhi : function() {
+    console.log(`Hello ${this.name}`);
+  }
+}
+console.log(person2);
+
+// 3. 함수 사용
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.sayhi = function() {
+    console.log(`Hello ${this.name}`);
+  }
+}
+let person3 = new Person("willy", 28);
+console.log(person3);
