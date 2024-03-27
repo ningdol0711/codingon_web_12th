3e8c2ec69aaf8d30148933ce567ec7ca0df363c2
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-03-27 11:43:00 +0900

    ejs

--- Study/06_NodeJs/12_express/index01.js
+++ Study/06_NodeJs/12_express/index01.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const server = express();
 
+server.set('view engine', 'ejs');
+server.set('views', 'views');
+server.use(express.static('public'));
+
 server.listen(8000, () => {
   console.log('server is running at 8000');
 });
 
-server.get((req, res) => {
-  res.render('/index', {title: 'Main'});
+server.get('/', (req, res) => {
+  res.render('index', {title: 'Main'});
 });
-server.get((req, res) => {
-  res.render('/about', {title: 'About'});
+server.get('/about', (req, res) => {
+  res.render('about', {title: 'About'});
 });
-server.get((req, res) => {
-  res.render('/create', {title: 'Create'});
+server.get('/create', (req, res) => {
+  res.render('create', {title: 'Create'});
+});
+
+server.use((req, res) => {
+  res.status(404).render('404');
 });
