3241133861241df58d885d48b9ec5d0d9cfd95d2
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-03-29 18:38:30 +0900

    home backup

--- Study/06_NodeJs/14_SYNC_ASYNC/task/task01.js
+++ Study/06_NodeJs/14_SYNC_ASYNC/task/task01.js
@@ -2,33 +2,26 @@ function call(name) {
   return new Promise((res, rej) => {
     setTimeout(() => {
       console.log('call');
-      console.log(name + '반가워');
-      res();
+      res(name);
     },1000)
   })
 }
 
 function back() {
   return new Promise((res, rej) => {
-    setTimeout(() => {
-      console.log('back');
-      console.log('back 을 실행했구나');
-      res();
+    setTimeout((txt) => {
+      console.log(`back`);
+      res('back');
     },1000)
   })
 }
 
 function hell() {
   return new Promise((res, rej) => {
-    setTimeout(() => {
-      console.log('여기는 callback 헬');
-      res();
+    setTimeout((message) => {
+      res('callback');
     },1000)
   })
 }
 
-call('Lee').then(() => {
-  return back();
-}).then(() => {
-  return hell();
-})
+call('Lee').then((name) => {console.log(`${name} 반가워!`);return back();}).then((txt) => {console.log(`${txt}을 실행했구나!`);return hell();}).then((message) => {console.log(`여기는 ${message} 헬`);})
--- Study/06_NodeJs/14_SYNC_ASYNC/task/task02.js
+++ Study/06_NodeJs/14_SYNC_ASYNC/task/task02.js
@@ -1,64 +1,36 @@
-// function call(name) {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       console.log('call');
-//       console.log(name + '반가워');
-//       res();
-//     },1000)
-//   })
-// }
-
-// function back() {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       console.log('back');
-//       console.log('back 을 실행했구나');
-//       res();
-//     },1000)
-//   })
-// }
-
-// function hell() {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       console.log('여기는 callback 헬');
-//       res();
-//     },1000)
-//   })
-// }
-
-// async function exec() {
-//   await call('Lee');
-//   await back();
-//   await hell();
-// }
-
-// exec();
-
-function call(name, cb) {
-  setTimeout(function () {
-      console.log(name);
-      cb(name);
-  }, 1000);
-}
-function back(cb) {
-  setTimeout(function () {
-      console.log('back');
-      cb('back');
-  }, 1000);
-}
-function hell(cb) {
-  setTimeout(function () {
-      cb('callback hell');
-  }, 1000);
-}
-
-call('Lee',(name) => {
-  console.log(name + "반가워");
-  back((txt) => {
-    console.log(txt + "을 실행했구나");
-    hell((message) => {
-      console.log("여기는" + message);
-    })
+function call(name) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log('call');
+      res(name);
+    },1000)
   })
-})
+}
+
+function back() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log('back');
+      res('back');
+    },1000)
+  })
+}
+
+function hell() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res('callback');
+    },1000)
+  })
+}
+
+async function exec() {
+  const name = await call('Lee');
+  console.log(name + ' 반가워!');
+  const txt = await back();
+  console.log(txt + '을 실행했구나!');
+  const message = await hell();
+  console.log(`여기는 ${message} 지옥...`);
+}
+
+exec();
