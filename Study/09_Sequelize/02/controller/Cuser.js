380b09052a2ccf8656d8aaa8e595e639b3ff4b90
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-04-12 13:26:56 +0900

    Sequelize

--- Study/09_Sequelize/02/controller/Cuser.js
+++ Study/09_Sequelize/02/controller/Cuser.js
@@ -11,24 +11,65 @@ exports.getSignin = (req, res) => {
 
 // 회원가입 요청
 exports.postSignup = (req, res) => {
-  
+  console.log('controller > Cuser.bodyjs > post_signup')
+  user.bodycreate({
+    userid: req.body.userid,
+    name: req.body.name,
+    pw: req.body.pw
+  }).then( result =>{
+    console.log('Created >', result)
+    res.send(result)
+  })
 }
 
 // 로그인 요청
 exports.postSignin = (req, res) => {
-
+  console.log('post방식으로 signin',req.body);
+  user.bodyfindOne({
+      raw: true,
+      where: {
+          userid: req.body.userid,
+          pw: req.body.pw
+      }
+  }).then((result) => {
+      console.log('findOne', result);
+      if (result) {
+          res.send({ isLogin: true, userInfo: result });
+      } else {
+          res.send({ isLogin: false });
+      }
+  })
 }
 
 // 회원정보 수정
 exports.postProfile = (req, res) => {
-
+  console.log('post방식으로 profile',req.body);
+  user.bodyfindOne({
+      where :{ userid: req.body.userid}
+  }).then(result =>{
+      console.log('findOne :post_profile > ', result)
+      if(result)   res.render('profile', {data: result})
+  })
 }
 
 // 회원정보 삭제
 exports.editProfile = (req, res) => {
-
+  console.log('controller > edit_profile' , req.body);
+  user.bodyupdate({
+      name:req.body.name,
+      pw: req.body.pw
+  },{
+      where : {id:req.body.id}
+  }).then( result =>{
+      res.send('회원정보수정')}
+  )
 }
 
 exports.deleteProfile = (req, res) => {
-
+  console.log('controller > delete_profile' , req.body);
+  user.bodydestrory({
+    where : {id:req.body.id}
+  }).then( result =>{
+    res.send({deleteId: req.body.id})}
+  )
 }
