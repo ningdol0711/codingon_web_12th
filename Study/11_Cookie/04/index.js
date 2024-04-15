c0176bfbaaecc5cd39f8e6ffd5c42cb7970c04d1
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-04-15 13:59:01 +0900

    cookie popup

--- Study/11_Cookie/04/index.js
+++ Study/11_Cookie/04/index.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const web = express();
-const PORT = 8000;
 
 const cookieConfig = {
   maxAge: 24 * 60 * 60 * 1000,
+  signed: true,
 }
 
-web.use(cookieParser());
 web.use(express.urlencoded({extended: true}));
 web.use(express.json());
+web.use(cookieParser('secret'));
 
 web.set('view engine', 'ejs');
 web.set('views', 'views');
 
 web.get('/', (req, res) => {
-  console.log(req.cookies.popup);
-  res.render('index', {popup: req.cookies.popup});
+  res.render('index', {popup: req.signedCookies.popup});
 })
 
 web.post('/setCookie', (req, res) => {
   res.cookie('popup', 'hide', cookieConfig);
-  res.send('success');
+  res.send('cookie set');
 })
 
-web.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}...`);
+web.listen(8000, () => {
+  console.log('Server is running at 8000....');
 })
