288c8240c93d0da89e8215d1b732fb02a40d33f8
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-04-16 12:52:03 +0900

    session

--- Study/12_Session/02/index.js
+++ Study/12_Session/02/index.js
@@ -2,11 +2,52 @@ const express = require('express');
 const session = require('express-session');
 const web = express();
 
-web.set('view enigne', 'ejs');
-web.set('views', 'views');
+const user = {id: 'banana', pw: '1234'};
 
+web.use('static', express.static(__dirname + '/static'));
 web.use(express.urlencoded({extended: true}));
 web.use(express.json());
+
+web.set('view engine', 'ejs');
+web.set('views', 'views');
+
+web.use(session({
+  secret: 'YHS',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    maxAge: 60 * 60 * 1000,
+  }
+}))
+
+web.get('/', (req, res) => {
+  res.render('index', {login: req.session.user, name: req.session.name});
+  console.log(req.session);
+})
+
+web.get('/login', (req, res) => {
+  res.render('login');
+})
+
+web.post('/', (req, res) => {
+  req.session.name = req.body.id;
+  if(JSON.stringify(req.body) == JSON.stringify(user)) {
+    req.session.user = 'logout';
+    res.render('index', {login: req.session.user, name: req.session.name});
+    console.log(req.session);
+  }
+  else {
+    res.send('Please check your ID, PW');
+  }
+})
+
+web.get('/logout', (req, res) => {
+  req.session.user = 'login';
+  req.session.name = req.body.id;
+  res.render('index', {login: req.session.user, name: req.session.name});
+  console.log(req.session);
+})
 
 web.listen(8000, () => {
   console.log('Server is running at 8000....');
