f1a1170a01d2ccbcde9fbd3aaed9987c50a1cfd4
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-04-27 13:21:06 +0900

    cart

---
+++ Study/15_Cart/02/src/data.js
@@ -0,0 +1,6 @@
+const shopItems = [
+  {id: 'cloth1', name: 'Casual shirt1', price: 151, desc: 'Lorem1', img: 'img/image-1.jpg'},
+  {id: 'cloth2', name: 'Casual shirt2', price: 152, desc: 'Lorem2', img: 'img/image-2.jpg'},
+  {id: 'cloth3', name: 'Casual shirt3', price: 153, desc: 'Lorem3', img: 'img/image-3.jpg'},
+  {id: 'cloth4', name: 'Casual shirt4', price: 154, desc: 'Lorem4', img: 'img/image-4.jpg'},
+]
