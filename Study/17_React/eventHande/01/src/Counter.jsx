5f9b5855770443025305e1a65729d38b634a7502
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-01 10:37:22 +0900

    react event handler

--- Study/17_React/eventHande/01/src/Counter.jsx
+++ Study/17_React/eventHande/01/src/Counter.jsx
@@ -3,11 +3,22 @@ import { useState } from "react";
 function FuncCount() {
   const [Number, setNumber] = useState(0);
 
+  const printAlert = (e) => {
+    alert(e);
+  }
+
+  const printConsole = (e, msg) => {
+    console.log(e, msg);
+  }
+
   return ( 
     <div className="App">
       <h2>{Number}</h2>
       <button onClick={() => {setNumber(Number + 1)}}>Increase</button>
       <button onClick={() => {setNumber(Number - 1)}}>Decrease</button>
+
+      <button onClick={() => {printAlert("Hello")}}>Alert print</button>
+      <button onClick={() => {printConsole("World")}}>Console print</button>
     </div>
    );
 }
