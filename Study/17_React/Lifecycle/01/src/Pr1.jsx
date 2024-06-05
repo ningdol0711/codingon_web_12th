32405f5025eb06dfa7d5d977c97de5863fa5d849
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-05 09:53:41 +0900

    Hooks

--- Study/17_React/Lifecycle/01/src/Pr1.jsx
+++ Study/17_React/Lifecycle/01/src/Pr1.jsx
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import PostsList from "./Pr1Child";
 
 function Pr1() {
-  let fakePosts;
-  
-  fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => fakePosts = json);
+  const [posts, setPosts] = useState([]);
 
-  console.log(fakePosts)
+  const getPosts = async () => {
+    const res = axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {});
+    setPosts(res.data.slice(0,10));
+  }
+  getPosts();
 
   return (
     <div className="postBox">
       <h2 className="listBox">Post list</h2>
-      <PostsList fakePosts={JSON.stringify(fakePosts)} />
+      <PostsList fakePosts={posts} />
     </div>
   );
 }
