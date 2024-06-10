74bfe8dfc2f7e8dfdcddbcdd28e96f590be8d36c
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-10 09:31:58 +0900

    react Redux

--- Study/17_React/Redux/01/src/index.js
+++ Study/17_React/Redux/01/src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FuncComp from './FuchComp';
+import ClassComp from './ClassComp';
+import FoodProps from './food';
+import BookComp from './book';
+import ConsoleComp from './Console';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App />
+    <FuncComp name="Codingon" />
+    <FuncComp name="Codingon">Hi</FuncComp>
+    <hr />
+    <ClassComp name="Lee"/>
+    <ClassComp />
+    <hr />
+    <FoodProps />
+    <FoodProps food="noodle"/>
+    <hr />
+    <BookComp title="나의 하루는 4시 40분에 시작된다" author="김유진" price="13,500원" type="에세이"/>
+    <hr /> */}
+    <ConsoleComp text="This is Text" vaild={() => {console.log("Console message")}}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
