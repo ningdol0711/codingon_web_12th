c7674223c390ac24212495380ace7ac259eafcea
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-11 09:45:20 +0900

    TypeScript

---
+++ Study/18_TypeScript/01/index.js
@@ -0,0 +1,7 @@
+console.log("Hi");
+var msg = "Hello";
+console.log(msg);
+function Plus(x, y) {
+    return x + y;
+}
+console.log(Plus(10, 20));
---
+++ Study/18_TypeScript/01/index.ts
@@ -0,0 +1,10 @@
+console.log("Hi");
+
+const msg: string = "Hello";
+console.log(msg);
+
+function Plus(x: number, y: number) {
+  return x + y;
+}
+
+console.log(Plus(10, 20));
