32dcb686d0daab0da50e048813f5fafda6bf8598
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-06-11 11:54:50 +0900

    TypeScript TypeFunc

--- Study/18_TypeScript/01/OnlyTsType.ts
+++ Study/18_TypeScript/01/OnlyTsType.ts
@@ -42,11 +42,63 @@ val = "Hi";
 
 // Interface
 interface Student {
-  name: string,
-  isPassed: boolean,
+  name: string;
+  isPassed: boolean;
 }
 
 const student1: Student = {
   name: "codingon",
   isPassed: true,
+};
+
+type Score = "A" | "B" | "C" | "D" | "E" | "F";
+let score1: Score = "A";
+
+type Score2 = 100 | 90 | 80 | 70 | 60;
+let score2: Score2 = 90;
+
+enum Score3 {
+  'A+' = 100,
+  'A' = 90,
+  'B+' = 80,
+  'B' = 70,
+}
+
+// interface 상속
+interface baseballClub extends Student {
+  position: string,
+  tall: number,
+  backNumber: number,
+  [grade: number]: Score3,
+}
+
+let player1: baseballClub = {
+  name: "otani",
+  isPassed: true,
+  position: "pitcher",
+  tall: 190,
+  backNumber: 17,
+  1: Score3.A,
+}
+
+console.log(player1)
+
+// 교차 타입 : 두개 이상의 타입을 합치는 것
+interface toy {
+  name: string,
+  start(): void,
+}
+interface car {
+  name: string,
+  color: string,
+  price: number,
+}
+
+type toycar = toy&car;
+
+const ToyCar: toycar = {
+  name: "bumblebee",
+  start() {console.log("Let's Go!!!")},
+  color: "blue",
+  price: 5000
 }
--- Study/18_TypeScript/01/Pr1.ts
+++ Study/18_TypeScript/01/Pr1.ts
@@ -8,3 +8,27 @@ olimpic_newgame = [{
 // olimpic_newgame[1] = false;
 
 console.log(olimpic_newgame);
+
+// interface Pr
+interface Game {
+  title: string,
+  price: number,
+  desc?: string,
+  category: string,
+  platform: string,
+}
+
+let heroGame_A: Game = {
+  title: 'DC 언체인드',
+  price: 50000,
+  desc: 'DC 히어로 & 빌런 각각의 개성은 물론, 액션의 재미까지!',
+  category: '액션',
+  platform: '모바일',
+}
+
+let heroGame_B: Game = {
+  title: 'MARVEL 퓨처파이트',
+  price: 65000,
+  category: '롤플레잉',
+  platform: '모바일',
+}
